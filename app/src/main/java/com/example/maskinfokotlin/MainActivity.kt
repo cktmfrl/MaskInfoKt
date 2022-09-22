6686a84c1e9e67b1b42cff48635e8f7c3013be96
origin <sskkccha@gmail.com>
2022-09-22 20:58:28 +0900

    권한 설정 수정 / 위치 정보 설정, 위치 업데이트 요청 추가 / anko 라이브러리 추가

--- app/src/main/java/com/example/maskinfokotlin/MainActivity.kt
+++ app/src/main/java/com/example/maskinfokotlin/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.maskinfokotlin
 
 import android.Manifest
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.LocationManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -15,6 +18,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.*
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -24,31 +28,21 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var storeAdapter: StoreAdapter
 
-    private val requestPermission = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { map ->
-        if (map[Manifest.permission.ACCESS_FINE_LOCATION]!!
-            && map[Manifest.permission.ACCESS_COARSE_LOCATION]!!) {
-            viewModel.fetchStoreInfo()
-        }
-    }
+//    private val requestPermission =
+//        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { map ->
+//            if (map[Manifest.permission.ACCESS_FINE_LOCATION]!!
+//                && map[Manifest.permission.ACCESS_COARSE_LOCATION]!!
+//            ) {
+//                viewModel.fetchStoreInfo()
+//                //performAction()
+//            } else {
+//                toast("권한이 거부되었습니다.")
+//            }
+//        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        if (ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            requestPermission.launch(arrayOf(
-                Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ))
-            return
-        }
 
         recycler_view.apply {
             layoutManager = LinearLayoutManager(this@MainActivity,
@@ -68,8 +62,76 @@ class MainActivity : AppCompatActivity() {
             })
         }
 
-        if (savedInstanceState == null) {
-            viewModel.fetchStoreInfo()
+        performAction()
+    }
+
+    private fun performAction() {
+        // 권한 설정
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            val permissions = arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
+            ActivityCompat.requestPermissions(this, permissions, 0)
+
+            return
+        }
+
+        // 위치 정보 설정
+        val locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+        if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+            && !locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+        ) {
+            alert("위치 서비스 설정이 꺼져 있어 현재 위치를 확인할 수 없습니다. 설정을 변경하시겠습니까?", "위치 정보 설정") {
+                yesButton {
+                    startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+                }
+                noButton {}
+            }.show()
+            return
+        }
+
+        viewModel.fetchStoreInfo()
+    }
+
+    // 권한 설정
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        when (requestCode) {
+            0 -> {
+                if (grantResults.isNotEmpty()) {
+                    var isAllGranted = true
+                    for (grant in grantResults) {
+                        if (grant != PackageManager.PERMISSION_GRANTED) {
+                            isAllGranted = false
+                            break
+                        }
+                    }
+
+                    if (isAllGranted) {
+                        performAction()
+                    } else {
+                        // Permission Denied
+                        // If you reject permission, you can not use this service\n\nPlease turn on permissions at [Setting] > [Permission]
+                        alert("권한을 거부할 경우 본 서비스를 이용하실 수 없습니다.\n\n[설정] > [권한]에서 권한을 켜주세요.") {
+                            okButton {
+                                // 애플리케이션 정보
+                                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+                                val uri: Uri = Uri.fromParts("package", packageName, null)
+                                intent.data = uri
+                                startActivity(intent)
+                            }
+                            cancelButton {
+                                toast("권한이 거부되었습니다.")
+                            }
+                        }.show()
+                    }
+                }
+
+            }
         }
     }
 
@@ -81,7 +143,7 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.action_refresh -> {
-                viewModel.fetchStoreInfo()
+                performAction()
                 true
             }
             else -> super.onOptionsItemSelected(item)
--- app/src/main/java/com/example/maskinfokotlin/MainViewModel.kt
+++ app/src/main/java/com/example/maskinfokotlin/MainViewModel.kt
@@ -2,6 +2,7 @@ package com.example.maskinfokotlin
 
 import android.annotation.SuppressLint
 import android.app.Application
+import android.os.Looper
 import androidx.hilt.Assisted
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.AndroidViewModel
@@ -11,6 +12,9 @@ import androidx.lifecycle.viewModelScope
 import com.example.maskinfokotlin.model.Store
 import com.example.maskinfokotlin.repository.MaskService
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
 import kotlinx.coroutines.launch
 
 class MainViewModel @ViewModelInject constructor(
@@ -29,7 +33,9 @@ class MainViewModel @ViewModelInject constructor(
 
         locationProvider.lastLocation
             .addOnSuccessListener { location ->
-                location?.let {
+                if (location == null) {
+                    startLocationUpdates(locationProvider)
+                } else { //location?.let {
                     viewModelScope.launch {
                         val storeInfo = service.fetchStoreInfo(location.latitude, location.longitude)
                         itemLiveData.value = storeInfo.stores.filter { store ->
@@ -42,4 +48,31 @@ class MainViewModel @ViewModelInject constructor(
                 }
             }
     }
+
+    @SuppressLint("MissingPermission")
+    private fun startLocationUpdates(locationProvider: FusedLocationProviderClient) {
+        val locationRequest = LocationRequest.create()
+
+        val locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                locationResult ?: return
+                for (location in locationResult.locations) {
+                    // Update UI with location data
+                    viewModelScope.launch {
+                        val storeInfo = service.fetchStoreInfo(location.latitude, location.longitude)
+                        itemLiveData.value = storeInfo.stores.filter { store ->
+                            store.remain_stat != null
+                        }
+
+                        // 로딩 끝
+                        loadingLiveData.value = false
+                    }
+                }
+            }
+        }
+
+        // 위치 업데이트 요청
+        locationProvider.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper())
+    }
+
 }
